07d925caeb41b2ac1b5a275847b97b552a92ab1d
Olivia <Oliviaparamour@gmail.com>
2020-12-08 19:17:05 +0800

    Rename variables and remove unused variables and functions.
    Removed program_end bool from find_fault().
    Renamed variables to be clearer.
    Removed switch_opcode function. Used as a dictionary instead.

--- day8.py
+++ day8.py
@@ -14,56 +14,47 @@ def load(file_name: str) -> list:
 def run(program: list) -> tuple:
     """runs program until it loops or reaches end of file."""
     step = 0
-    acc = 0
+    accumulator = 0
     instructions = []
     while True:
         if step not in instructions and step < len(program):
             instructions.append(step)
             current = program[step]
             if current["opcode"] == "acc":
-                acc += current["value"]
+                accumulator += current["value"]
             if current["opcode"] == "jmp":
                 step += current["value"]
             else:
                 step += 1
         else:
             instructions.append(step)
-            return acc, instructions
+            return accumulator, instructions
 
 def find_fault(program: list, executed_instructions: list) -> tuple:
     """iterates through all executed instructions to test and find the fault"""
-    program_end = False
+    switch_opcode = {"jmp":"nop", "nop":"jmp"}
     modified_program = program.copy()
     for i in executed_instructions: 
         opcode = modified_program[i]["opcode"]
-        if opcode != "acc": 
-            opcode = switch_opcode(opcode)
-            modified_program[i].update({"opcode": opcode})
-            acc, instructions = run(modified_program)
+        if opcode != "acc":
+            modified_program[i].update({"opcode": switch_opcode[opcode]})
+            accumulator, instructions = run(modified_program)
             if instructions[-1] >= len(program):
-                return acc, i
-            modified_program[i].update({"opcode": switch_opcode(opcode)})
+                return accumulator, i
+            modified_program[i].update({"opcode": opcode})
     return None, None
-
-def switch_opcode(opcode: str) -> str:
-    if opcode == "jmp":
-        return "nop"
-    elif opcode == "nop":
-        return "jmp"
-    else:
-        return opcode
 
 
 def main() -> None:
     """The main function for day 8. Answers Question 1 and 2."""
     program = load("day8.txt")
-    acc, instructions = run(program)
-    acc2, instruction = find_fault(program, instructions)
+    accumulator, instructions = run(program)
+    fixed_accumulator, instruction = find_fault(program, instructions)
     
     print(f"Total number of inputs: {len(program)}")
-    print(f"value in accumulator when looping: {acc}")
+    print(f"value in accumulator when looping: {accumulator}")
     print(f"Corrupted instruction: {instruction}")
-    print(f"value in accumulator when fixed: {acc2}")
+    print(f"value in accumulator when fixed: {fixed_accumulator}")
     # [print(input) for input in program]
 
 if __name__ == "__main__":
