7d4391f30a8b877b2acd0fdcc9692a5bddbed7c2
Olivia <Oliviaparamour@gmail.com>
2020-12-10 15:18:25 +0800

    Add dummy function to template.py
    Added a dummy function that to template to make it quicker to write
    solutions to questions 1 and 2 for the day.

--- template.py
+++ template.py
@@ -11,12 +11,18 @@ def load(file_name: str) -> list:
             input.append(line.strip())
     return input
 
+def myfunc(input: list) -> int:
+    return 0
+
+
 def main() -> None:
-    """The main function for day 4. Answers Question 1 and 2."""
+    """The main function for day 10. Answers Question 1 and 2."""
     inputs = load("day5.txt")
-    
+    value = myfunc(inputs)
+
     print(f"Total number of inputs: {len(inputs)}")
     [print(input) for input in inputs]
+    print(f"The value: {value}")
 
 if __name__ == "__main__":
     main()
